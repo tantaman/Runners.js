773a731a9724038bb42db5b31cf6aa846370d754
Matt Crinklaw-Vogt <matt.crinklaw@gmail.com>
2013-01-02 15:02:10 -0500

    exports for different environments

--- dist/Runners.js
+++ dist/Runners.js
@@ -702,11 +702,13 @@ AbstractPWorkerPool.prototype = {
 	}
 };
 
-;window.Runners = workerFactory;
-
 if (typeof define !== 'undefined') {
 	define(function() {
 		return workerFactory;
 	});
+} else if (typeof exports !== 'undefined') {
+	exports = workerFactory;
+} else {
+	window.Runners = workerFactory;	
 }
 }(this));
--- src/Runners.js
+++ src/Runners.js
@@ -1,7 +1,9 @@
-;window.Runners = workerFactory;
-
 if (typeof define !== 'undefined') {
 	define(function() {
 		return workerFactory;
 	});
+} else if (typeof exports !== 'undefined') {
+	exports = workerFactory;
+} else {
+	window.Runners = workerFactory;	
 }
